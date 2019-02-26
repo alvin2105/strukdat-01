ca972c5b13f54fe06cd7cc38d123846653d92683
alvin <47733168+alvin2105@users.noreply.github.com>
2019-02-26 09:52:35 +0700

    Add files via upload

---
+++ SWAP.cpp
@@ -0,0 +1,22 @@
+/*nama		: Alvin
+NPM			: 140810180013
+kelas		: A
+tanggal 	: 26 februari 2019
+deskripsi	: swap
+*/
+#include<iostream>//file header
+using namespace std;//
+void swap(){
+	
+}
+int main(){
+	int x=2;
+	int y=3;
+	cout<<"SEBELUM DISWAP "<<endl;
+	cout<<"x adalah	: "<<x<<endl;
+	cout<<"y adalah	: "<<y<<endl;
+	swap(x,y);
+	cout<<"SETELAH DISWAP "<<endl;
+	cout<<"x adalah	: "<<x<<endl;
+	cout<<"y adalah	: "<<y<<endl;
+}
