2e70dbecda8c8702e9563dbd2c4ab01ce91d93f3
alvin <47733168+alvin2105@users.noreply.github.com>
2019-02-26 10:18:03 +0700

    strukdat-01

---
+++ SWAP.cpp
@@ -0,0 +1,22 @@
+/*nama		: Alvin
+NPM			: 140810180013
+kelas		: A
+tanggal 	: 26 februari 2019
+deskripsi	: swap
+*/
+#include<iostream>//file header
+using namespace std;//
+void swap(){
+	
+}
+int main(){
+	int x=2;//inisialisasi
+	int y=3;//inisialisasi
+	cout<<"SEBELUM DISWAP "<<endl;//kondisi sebelum di swap
+	cout<<"x adalah	: "<<x<<endl;
+	cout<<"y adalah	: "<<y<<endl;
+	swap(x,y);//fungsi swap
+	cout<<"SETELAH DISWAP "<<endl;//kondisi setelah diswap
+	cout<<"x adalah	: "<<x<<endl;
+	cout<<"y adalah	: "<<y<<endl;
+}
